64789c68dd9318f045e7b9224116a33199e98a3b
CamilleMrntz <94170738+CamilleMrntz@users.noreply.github.com>
2022-04-21 17:28:13 +0200

    Add files via upload

---
+++ library/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Librairie</title>
+</head>
+
+
+<body>
+    <section class="section-index">
+        <div class="left-side">
+            <img src="" alt="logo-site" class="img-accueil">
+
+            <form action="" method="post" class="form-connexion">
+                <input type="email" name="email" id="email" placeholder="E-mail" required>
+                <input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
+                <input class="btn-connexion" type="submit" value="Se connecter">
+            </form>
+            <a href="formulaire.php"><button class="btn-inscription">S'inscrire</button></a>
+        </div>
+       
+        <img src="img/img-index.jpg" class="img-right">
+    </section>
+    
+</body>
+</html>
